9a3836aa44fa98d6671c310453ea243f040555e9
ChinmaySharma675 <chinmay.sharma675@gmail.com>
2026-02-02 10:21:33 +0530

    ir code

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,18 @@
-//
-// Created by chinm on 02-02-2026.
-//
+#include <Arduino.h>
+#define irPin 2
+void setup(){
+    pinMode(irPin,INPUT);
+}
+void loop()
+{
+    int irValue = digitalRead(irPin);
+    if (irValue == HIGH)
+    {
+        Serial.println("IR Sensor:No Obstacle");
+    }else
+    {
+        Serial.println("IR Sensor:Obstacle Detected");
+    }
+    delay(500); //Delay for readability
+
+}
